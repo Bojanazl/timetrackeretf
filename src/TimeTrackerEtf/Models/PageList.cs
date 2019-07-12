acd4c1d49e00537eb515a2941043978b12707129
bojana@lujic.net <bojana@lujic.net>
2019-07-12 16:34:15 +0200

    Add view and input models

---
+++ src/TimeTrackerEtf/Models/PageList.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TimeTrackerEtf.Models
+{
+    public class PageList<T>
+    {
+
+        public IEnumerable<T>Items { get; set; }
+
+        public int Page { get; set; } //trenutna stranica
+
+        public int PageSize { get; set; } //koliko itema stranca vraca
+
+        public int TotalCount { get; set; } //vraca PageSize
+
+        public int TotalPages =>
+            (int) Math.Ceiling (TotalCount / (decimal) PageSize); //koliko ukupno ima stranica. Paginacija.
+    }
+}
---
+++ src/TimeTrackerEtf/Models/ProjectModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TimeTrackerEtf.Models
+{
+    public class ProjectModel
+    {
+
+        public long Id { get; set; }
+
+        public string Name { get; set; }
+
+        public long ClientId { get; set; }
+
+        public string ClientName { get; set; }
+    }
+}
